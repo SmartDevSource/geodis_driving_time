c7bf6347d700e331e047b66f8cbbcdd8b1137568
SmartDevSource <emmanuel.beaugendre@outlook.fr>
2024-07-14 10:49:06 +0200

    added dee

--- driving_time/src/components/calc_button.jsx
+++ driving_time/src/components/calc_button.jsx
@@ -1,16 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { getPathData } from "../functions/path_data"
 
-export const CalcButton = ({first, second, sheetPlan, onHandleOutput}) => {
+import dee_image from '../assets/dee.png'
+import dee_audio from '../assets/dee.ogg'
 
+export const CalcButton = ({first, second, sheetPlan, onHandleOutput}) => {
     const [firstOutput, setFirstOutput] = useState(null)
     const [secondOutput, setSecondOutput] = useState(null)
+    const [showDee, setShowDee] = useState(false)
+    const audio = new Audio()
+    audio.src = dee_audio
+    audio.volume = 1
 
     const handleClick = () => {
         const first_output = getPathData({data: first, sheetPlan: sheetPlan})
         const second_output = getPathData({data: second, sheetPlan: sheetPlan})
         setFirstOutput(first_output)
         setSecondOutput(second_output)
+        setShowDee(true)
+        setTimeout(() => {
+            setShowDee(false)
+        }, 3000);
+        audio.play()
     }
 
     useEffect(()=>{
@@ -21,7 +32,12 @@ export const CalcButton = ({first, second, sheetPlan, onHandleOutput}) => {
 
     return (
         <>
-            <button className="stylized_button"
+            {showDee &&
+            <span className="dee_span">
+                <img src={dee_image}></img>
+            </span>
+             }
+            <button className = "stylized_button"
                 onClick={handleClick}>
                 Calculer le gain
             </button>
