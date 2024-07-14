5694eebf3caf1629ec075b8e324523591ba64258
SmartDevSource <emmanuel.beaugendre@outlook.fr>
2024-07-14 22:03:24 +0200

    changed navbar format typo

--- driving_time/src/components/Navbar.jsx
+++ driving_time/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ export const Navbar = ({handleClick}) => {
     return (
         <>
             <div className="navbar">
-                <button className="link_button" style={{color: 'white'}} onClick={()=>handleClick("compare_ways")}>Comparer deux chemins</button>
-                <button className="link_button" style={{color: 'white'}} onClick={()=>handleClick("average_missions")}>Moyenne des missions</button>
+                <button className="link_button" style={{color: 'white'}} onClick={()=>handleClick("compare_ways")}>COMPARER DEUX CHEMINS</button>
+                <button className="link_button" style={{color: 'white'}} onClick={()=>handleClick("average_missions")}>MOYENNE DES MISSIONS</button>
             </div>
         </>
     )
