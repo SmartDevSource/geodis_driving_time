0252de073d35f267d0c31bd8f040133d7468b4b6
SmartDevSource <emmanuel.beaugendre@outlook.fr>
2024-08-19 20:49:15 +0200

    updated format calcs

--- driving_time/src/components/FormatButton.jsx
+++ driving_time/src/components/FormatButton.jsx
@@ -5,11 +5,14 @@ export const FormatButton = ({first, second, handleFormatedFirst, handleFormated
             const first_copy = first.split('\n').map(element=>{
                 if (checkFormat(element)){
                     const splitted = element.split('-')
-                    if (splitted[0] != 'E03'){
-                        return `${splitted[0]}-${splitted[1]}-${splitted[2][0]}00`
-                    } else {
-                        return element
-                    }
+                    const last_part = splitted[2].slice(1)
+                    console.log(last_part)
+                        if (isNaN(last_part) || +last_part >= 10){
+                            return `${splitted[0]}-${splitted[1]}-${splitted[2][0]}00`
+                        } else {
+                            return element
+                        }
+
                 } else {
                     return element
                 }
@@ -20,11 +23,14 @@ export const FormatButton = ({first, second, handleFormatedFirst, handleFormated
             const second_copy = second.split('\n').map(element=>{
                 if (checkFormat(element)){
                     const splitted = element.split('-')
-                    if (splitted[0] != 'E03'){
-                        return `${splitted[0]}-${splitted[1]}-${splitted[2][0]}00`
-                    } else {
-                        return element
-                    }
+                    const last_part = splitted[2].slice(1)
+                    console.log(last_part)
+                        if (isNaN(last_part) || +last_part >= 10){
+                            return `${splitted[0]}-${splitted[1]}-${splitted[2][0]}00`
+                        } else {
+                            return element
+                        }
+
                 } else {
                     return element
                 }
