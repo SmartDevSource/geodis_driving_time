1e370236b1a22c2973290cdab13681faa290d2a9
SmartDevSource <emmanuel.beaugendre@outlook.fr>
2024-11-27 09:49:12 +0100

    .

--- driving_time/src/components/Navbar.jsx
+++ driving_time/src/components/Navbar.jsx
@@ -2,8 +2,20 @@ export const Navbar = ({handleClick}) => {
     return (
         <>
             <div className="navbar">
-                <button className="link_button" style={{color: 'white'}} onClick={()=>handleClick("compare_ways")}>COMPARER DEUX CHEMINS</button>
-                <button className="link_button" style={{color: 'white'}} onClick={()=>handleClick("average_missions")}>MOYENNE DES MISSIONS</button>
+                <button 
+                    className="link_button"
+                    style={{color: 'white', fontWeight: 'normal'}}
+                    onClick={()=>handleClick("compare_ways")}
+                >
+                    COMPARER DEUX CHEMINS
+                </button>
+                <button
+                    className="link_button"
+                    style={{color: 'white', fontWeight: 'normal'}}
+                    onClick={()=>handleClick("average_missions")}
+                >
+                    MOYENNE DES MISSIONS
+                </button>
             </div>
         </>
     )
